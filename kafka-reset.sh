d78ca48ee8db88747ff641cbd2b4c3300d2fc7ac
Mark <inyabass@gmail.com>
2021-08-23 23:19:31 +0100

    Parameterise disk

--- kafka-reset.sh
+++ kafka-reset.sh
@@ -1,10 +1,11 @@
 #!/usr/bin/env bash
+# $1 = machine $2 = disk
 docker stop kafka-1
 docker rm kafka-1
 docker stop zookeeper-1
 docker rm zookeeper-1
-rm -fR /c/zookeeper/*
-rm -fR /c/kafka/*
+rm -fR /$2/zookeeper/*
+rm -fR /$2/kafka/*
 ./kafka.sh $1
 sleep 1
 ./create-topics.sh
