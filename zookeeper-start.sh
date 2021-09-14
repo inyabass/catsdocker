dcfdf520319955068cd8839357d259e6d0a68eed
Mark Wilkinson <mark.wilkinson@dotmatics.com>
2021-09-14 18:03:28 +0100

    Move zookeeper admin to 8081

--- zookeeper-start.sh
+++ zookeeper-start.sh
@@ -3,4 +3,4 @@
 export ALLOW_ANONYMOUS_LOGIN
 export ZOO_SERVER_ID
 export ZOO_SERVERS
-docker run -d --name $ZOOKEEPER_CONTAINER_NAME --rm -p 2181:2181 -p 2888:2888 -p 3888:3888 -p 8080:8080 -v $ZOOKEEPER_STORAGE_VOLUME:/bitnami/zookeeper -e ALLOW_ANONYMOUS_LOGIN -e ZOO_SERVER_ID -e ZOO_SERVERS bitnami/zookeeper:latest
+docker run -d --name $ZOOKEEPER_CONTAINER_NAME --rm -p 2181:2181 -p 2888:2888 -p 3888:3888 -p 8081:8080 -v $ZOOKEEPER_STORAGE_VOLUME:/bitnami/zookeeper -e ALLOW_ANONYMOUS_LOGIN -e ZOO_SERVER_ID -e ZOO_SERVERS bitnami/zookeeper:latest
