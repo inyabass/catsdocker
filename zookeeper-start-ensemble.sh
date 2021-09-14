8d77ff0cc4a820b858e7a40774d2618dd220d8e0
Mark <inyabass@gmail.com>
2021-09-15 00:11:26 +0100

    Add zookeeper ensemble

---
+++ zookeeper-start-ensemble.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+. library.sh
+export ALLOW_ANONYMOUS_LOGIN
+export ZOO_SERVER_ID="$ZOO_SERVER_ID_ENSEMBLE"
+export ZOO_SERVERS="$ZOO_SERVERS_ENSEMBLE"
+echo Starting with ID $ZOO_SERVER_ID servers $ZOO_SERVERS
+docker run -d --name $ZOOKEEPER_CONTAINER_NAME --rm -p 2181:2181 -p 2888:2888 -p 3888:3888 -p 8081:8080 -v $ZOOKEEPER_STORAGE_VOLUME:/bitnami/zookeeper -e ALLOW_ANONYMOUS_LOGIN -e ZOO_SERVER_ID -e ZOO_SERVERS bitnami/zookeeper:latest
